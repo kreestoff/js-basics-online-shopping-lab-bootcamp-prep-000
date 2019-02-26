2e4542d63c2cd0ab125fab76cd820af1d9ded4f4
Christopher McIntosh <christopher.l.mcintosh@outlook.com>
2019-02-26 21:43:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,11 +10,18 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ cart.push({itemName: item, itemPrice: Math.floor(Math.random() * 100) + 1})
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    return "Your shopping cart is empty."
+  }
+  var yourCart = []
+  for (let i = 0; i < cart.length; i++) {
+    
+  }
 }
 
 function total() {
